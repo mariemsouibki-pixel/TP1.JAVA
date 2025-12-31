66fef0400ef50367aebc305107f2636a71b01e4c
souibki mariem <mariem.souibki@isgb.ucar.tn>
2025-12-31 18:25:30 +0100

    Rename class from ex1 to EX1 and adjust package

--- ex1.java
+++ ex1.java
@@ -1,10 +1,11 @@
 package tp1;
 
-public class ex1 {public static void main(String[] args) {
+public class EX1 {public static void main(String[] args) {
     System.out.println("Je suis un génie");
     System.out.println(); 
     System.out.println("Je sais écrire un programme \"JAVA\"");
 }
 }
+
 
 
